a911c2d0b677cfe97abd541973b88755bc640501
yue <yue@172.26.200.42>
2017-01-04 19:45:20 -0500

    commit

--- Test/Check_Ports/check_sled_ports.py
+++ Test/Check_Ports/check_sled_ports.py
@@ -1,17 +1,6 @@
 
 import os
 
-class test1(object):
-    def __init__(self):
-        self.a = 1
-        
-    def __enter__(self):
-        print ("enter")
-        return self.a
-    
-    def __exit__(self,type,value,TypeError):
-        print ("exit")
-        return True
 
 project_dir = "D:\\svn\\k450\\design\\analog\project\\"
 cell_path = "D:\\svn\\k450\\design\\analog\\libraries\\acr_lib\\acr_afe_top\\acr_afe_top.asc"
@@ -23,9 +12,8 @@ with open(cell_path) as f:
     lines = [line.rstrip() for line in f.readlines()]
     
 
-print (len(lines))
+
 for line in lines:
     print(line)
     
-with test1() as t:
-    print(t)      
+    
